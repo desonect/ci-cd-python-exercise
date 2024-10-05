bb06676853c262da9844cbfe80aae06156b7be55
Desonect <gabriel.jaen@a.fecaf.com.br>
2024-10-05 17:48:41 -0300

    sei lá, voltando do início

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,6 @@ from src.main import soma
 class TestMain(unittest.TestCase):
     def test_soma(self):
         self.assertEqual(soma(3, 5), 8)
-    
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     unittest.main()
